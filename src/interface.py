2071312261229853cbaa6e5e62ab40df1a1a4f57
Deun Lee <deunlee@deunlee.com>
2023-04-14 15:24:04 +0900

    Update interface menu

--- src/interface.py
+++ src/interface.py
@@ -1,8 +1,6 @@
 import os
-from typing import Type
 import scapy.all as scapy
-from scapy.all import get_working_if, ltoa, conf
-from scapy.interfaces import NetworkInterface
+from scapy.interfaces import NetworkInterface, NetworkInterfaceDict
 
 from src.utils import *
 
@@ -15,12 +13,44 @@ class NetForge_Interface:
     def get_selected_interface(self) -> NetworkInterface | None:
         return self.interface
 
-
     #========================= UTILS =========================#
 
+    @classmethod
+    def get_valid_interfaces(cls) -> list[NetworkInterface]:
+        data: NetworkInterfaceDict | None = scapy.conf.ifaces.data
+        result = []
+        for interface_id in data:
+            interface = data[interface_id]
+            if interface.is_valid() and interface.mac != '':
+                result.append(interface)
+        return sorted(result, key=lambda x: x.description)
 
-    def get_gateway_address(self, interface: NetworkInterface) -> str | None: # (ex: '192.168.0.1')
-        routes = get_unicast_routes()
+
+    @classmethod
+    def get_unicast_routes(cls):
+        data = scapy.conf.route.routes # [(Network: int, Netmask: int, Gateway: str, Iface: str, OutputIP: str, Metric: int)]
+        result = []
+        for route in data:
+            netmask = scapy.ltoa(route[1])
+            if netmask == '255.255.255.255' or netmask == '240.0.0.0':
+                continue # Skip broadcast and multicast
+            result.append(route)
+        return result
+
+
+    @classmethod
+    def get_network_address_with_cidr(cls, interface: NetworkInterface) -> str | None: # (ex: '192.168.0.0/24')
+        routes = cls.get_unicast_routes()
+        routes = list(filter(lambda x: x[3] == interface.network_name and x[2] == '0.0.0.0', routes))
+        # '0.0.0.0' for connected route
+        if len(routes):
+            return f'{ltoa(routes[0][0])}/{bin(routes[0][1]).count("1")}'
+        return None
+
+
+    @classmethod
+    def get_gateway_address(cls, interface: NetworkInterface) -> str | None: # (ex: '192.168.0.1')
+        routes = cls.get_unicast_routes()
         for route in routes:
             if route[2] == '0.0.0.0':
                 continue # Skip connected route
@@ -32,8 +62,71 @@ class NetForge_Interface:
     def get_gateway_address_of_selected_interface(self) -> str | None:
         return self.get_gateway_address(self.select_interface)
 
-
     #========================= MENUS =========================#
+
+    def menu(self) -> None:
+        def print_menu():
+            selected = self.interface.description if self.interface else 'not selected'
+            selected = str_fixed_len(selected, 52)
+            info = ''
+            if self.interface:
+                network = self.get_network_address_with_cidr(self.interface)
+                gateway = self.get_gateway_address(self.interface)
+                info = '\n |   Network: {:18}      Gateway: {:15}       |'.format(
+                    network if network else 'no network', gateway if gateway else 'no gateway'
+                )
+            print(f'''
+ +-------------------------------------------------------------------+
+ |                        < Interface Menu >                         |
+ +-------------------------------------------------------------------+
+ |   Interface: {selected} |{info}
+ +-------------------------------------------------------------------+
+ |                                                                   |
+ |   (1) Select Interface             (2) Show Routing Table         |
+ |                                                                   |
+ |   (3) Show Forwarding Status                                      |
+ |   (4) Enable Forwarding            (5) Disable Forwarding         |
+ |                                                                   |
+ |   (0) Back to Main Menu                                           |
+ |                                                                   |
+ +-------------------------------------------------------------------+\n''')
+
+        print_menu()
+        while True:
+            try:
+                i = input('[?] Enter menu number : ')
+            except KeyboardInterrupt:
+                print()
+                break
+            print()
+
+            if i == '1':
+                self.select_interface()
+            elif i == '2':
+                print(scapy.conf.route)
+                print()
+            elif i == '3':
+                self.show_forwarding_status()
+            elif i == '4':
+                if not self.interface:
+                    print('[-] Please select an interface first.\n')
+                else:
+                    self.set_forwarding_status(True, self.interface)
+                    print()
+            elif i == '5':
+                self.set_forwarding_status(False)
+                print()
+            elif i == '0':
+                break
+            else:
+                continue
+
+            try:
+                input('[?] Press enter key to back to menu. ')
+            except KeyboardInterrupt:
+                print()
+            print_menu()
+
 
     def show_forwarding_status(self) -> None:
         if is_windows():
@@ -45,18 +138,24 @@ class NetForge_Interface:
                 | Sort-Object -Property
                     @{Expression = 'ConnectionState'; Descending = $true},
                     @{Expression = 'InterfaceAlias'; Descending = $false}
-                | Format-Table''')
+                | Format-Table''', hide_output=False)
         elif is_linux():
+            os.system('cat /proc/sys/net/ipv4/ip_forward')
+            os.system('sysctl -a | grep net.ipv4.ip_forward')
+
             print('[-] Currently Linux is not supported.')
         elif is_macos():
             print('[-] Currently MacOS is not supported.')
 
 
-    def set_forwarding_status(self, interface_index: int, status: bool) -> bool:
+    def set_forwarding_status(self, status: bool, interface: NetworkInterface | None = None) -> bool:
         if is_windows():
-            ok = run_powershell(f'''Set-NetIPInterface 
-                -InterfaceIndex {interface_index}
-                -Forwarding {"Enabled" if status else "Disabled"}''')
+            if interface == None:
+                ok = run_powershell(f'''Set-NetIPInterface -Forwarding {"Enabled" if status else "Disabled"}''')
+            else:
+                ok = run_powershell(f'''Set-NetIPInterface 
+                    -InterfaceIndex {interface.index}
+                    -Forwarding {"Enabled" if status else "Disabled"}''')
         elif is_linux():
             # echo 1 > /proc/sys/net/ipv4/ip_forward
             # sysctl -w net.ipv4.ip_forward=1
@@ -78,26 +177,15 @@ class NetForge_Interface:
         return False
 
 
-    def select_interface(self): # NetworkInterface | None
+    def select_interface(self) -> None:
         # powershell -command "Get-NetAdapter"
-        selected_interface = None
+        # powershell -command "Get-NetIPInterface"
+        print('[*] List of network interfaces : \n')
         
-        def get_network_interfaces():
-            data   = scapy.conf.ifaces.data # NetworkInterfaceDict
-            result = []
-            for id in data:
-                device = data[id]
-                if device.is_valid() and device.mac != '':
-                    result.append(device)
-            return result
-
-        print('[*] List of interfaces :')
-        print()
-        
-        interfaces        = sorted(get_network_interfaces(), key=lambda x: x.description)
-        default_interface = get_working_if() # NetworkInterface
+        interfaces        = self.get_valid_interfaces()
+        default_interface = scapy.get_working_if() # NetworkInterface
         default_index     = default_interface.index if default_interface else 0
-        routes            = get_unicast_routes()
+        routes            = self.get_unicast_routes()
 
         print('{:^5}  {:42}  {:17}  {:15}  {}'.format('Index', 'Interface', 'MAC', 'IPv4', 'Gateway'))
         print('=' * 110)
@@ -106,35 +194,32 @@ class NetForge_Interface:
             gateway = list(map(lambda x: 'Connected' if x[2] == '0.0.0.0' else x[2], 
                         filter(lambda x: x[3] == interface.network_name, routes)))
             print('{:>4}   {:42}  {:17}  {:15}  {}'.format(
-                interface.index, desc, interface.mac, interface.ip,
-                ', '.join(gateway) + (' (DEFAULT)' if interface.index == default_index else '')
+                interface.index, desc, interface.mac, interface.ip, ', '.join(gateway)
             ))
         print('=' * 110)
         print()
         
         while True:
             try:
-                idx = input(f'[?] Please enter the interface number to use ({default_index}) : ').strip()
+                index = input(f'[?] Please enter the interface number to use ({default_index}) : ').strip()
             except KeyboardInterrupt:
                 print()
-                return None
-
-            if idx == '': idx = str(default_index)
-            if not idx.isdigit():
+                self.interface = None
+                return
+            if index == '': index = str(default_index)
+            if not index.isdigit():
                 print('\n[-] Please enter a number.\n')
-                return None
+                continue
             try:
-                selected_interface = conf.ifaces.dev_from_index(int(idx))
+                self.interface = scapy.conf.ifaces.dev_from_index(int(index))
                 break
             except:
                 print('\n[-] Please check the interface number.\n')
 
         print()
-        print(f'[+] The selected interface is "{selected_interface.description}".\n')
-        
-        flags = str(interface.flags).replace('+', ', ')
-        print(f'[+] NetName : {selected_interface.network_name}')
-        print(f'[+] Flags   : {flags}\n')
-        
-        return selected_interface
+        print(f'[+] Interface : {self.interface.description}')
+        print(f'[+] Alias     : {self.interface.name}')
+        print(f'[+] Net Name  : {self.interface.network_name}')
+        print(f'[+] Flags     : {str(interface.flags).replace("+", ", ")}\n')
+
 
